1337af0ac94038084d27f6d9bc17a0d042e32936
Weiss Bai <whitesgr03@gmail.com>
2022-09-11 19:10:07 +0800

    Add close button event listener

--- book.js
+++ book.js
@@ -1,8 +1,12 @@
 'use strict'
 
+const formBgWrap = document.querySelector('.wrap')
 const addBookButton = document.querySelector('.addBookButton')
+const closeButton = document.querySelector('.closeButton')
 
+formBgWrap.addEventListener('click', showField)
 addBookButton.addEventListener('click', showField)
+closeButton.addEventListener('click', showField)
 function showField(e) {
     if (e.target === this) {
         formBgWrap.classList.toggle('active')
