d4e6b44fccc938dcea57af8c16abb38f6e2a5c36
Weiss Bai <whitesgr03@gmail.com>
2022-09-11 19:05:43 +0800

    Add showField Fn

--- book.js
+++ book.js
@@ -3,6 +3,11 @@
 const addBookButton = document.querySelector('.addBookButton')
 
 addBookButton.addEventListener('click', showField)
+function showField(e) {
+    if (e.target === this) {
+        formBgWrap.classList.toggle('active')
+    }
+}
 
 function showField() {
     const wrap = document.querySelector('.wrap');
