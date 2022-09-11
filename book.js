842a5e2dd9a84b0d7e17bad07783028eb0c70de6
Weiss Bai <whitesgr03@gmail.com>
2022-09-11 19:27:05 +0800

    Add "createTemplate" Fn to create the .book element

--- book.js
+++ book.js
@@ -34,8 +34,59 @@ function showField(e) {
     }
 }
 
-function showField() {
-    const wrap = document.querySelector('.wrap');
-    wrap.classList.add('active')
+function createElement(element, className, text, id) {
+    let el = document.createElement(element);
+    if (className) {
+        el.classList.add(...className);
+    }
+    if (text) {
+        el.textContent = text;
+    }
+    if (id) {
+        el.dataset.id = id;
+    }
+    return el
+}
+
+function createTemplate(book) {
+    const wrap = document.querySelector('.library');
+    const props = [];
+
+    for (let key in book) {
+        props.push(key)
+    }
+    // first div
+    const parent = createElement('div', ['book'], null, book.id)
+
+    let child = null;
+    let div = createElement('div')
+    child = createElement('h2', [props[0]], book.title)
+    div.appendChild(child)
+
+    child = createElement('h3', [props[1]], book.author)
+    div.appendChild(child)
+
+    child = createElement('p', [props[2]], `Print length: ${book.pages} pages`)
+    div.appendChild(child)
+
+    parent.appendChild(div)
+
+    // second div
+    div = createElement('div')
+
+    child = createElement(
+        'button', 
+        book.isRead ? [props[3], 'finished'] : [props[3]],
+        book.isRead ? 'Read' : 'Not read',
+    )
+    div.appendChild(child)
+
+    child = createElement('button', ['removeBook'], 'Remove', book.id)
+    div.appendChild(child)
+
+    parent.appendChild(div)
+
+    wrap.appendChild(parent)
+}
 }
 
