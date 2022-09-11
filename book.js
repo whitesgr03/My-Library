0c79d5a2fe66f81591aae3c85f0757a30ce7dedb
Weiss Bai <whitesgr03@gmail.com>
2022-09-11 19:16:59 +0800

    Create class book

--- book.js
+++ book.js
@@ -7,6 +7,27 @@ const closeButton = document.querySelector('.closeButton')
 formBgWrap.addEventListener('click', showField)
 addBookButton.addEventListener('click', showField)
 closeButton.addEventListener('click', showField)
+class Book{
+    constructor({ title, author, pages, isRead}) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isRead = isRead;
+
+        if (myLibrary.length > 0) {
+            this.id = myLibrary.at(-1).id + 1;
+        } else {
+            this.id = 1;
+        }
+    }
+    info() {
+        return `The Hobbit by ${this.author} ${this.title}, ${this.pages} pages, ${this.read}`
+    }
+    changeStatus() {
+        this.isRead = !this.isRead
+    }
+} 
+
 function showField(e) {
     if (e.target === this) {
         formBgWrap.classList.toggle('active')
